dd6c57773f714891e0fb60eb95d52abca7238db3
Koshelndra <koshelndra31@gmail.com>
2019-07-30 01:17:51 +0530

    Added percentage and seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,19 @@ class User < ApplicationRecord
   has_many :questions, through: :answers
 
   def correct_answers(all_questions)
-    unskipped_answers.where(options: {correct: true, question_id:  all_questions.ids}).count
+    (unskipped_answers.where(options: {correct: true, question_id:  all_questions.ids}).count.to_f * 100) / all_questions.count.to_f
   end
 
   def wrong_answers(all_questions)
-    unskipped_answers.where(options: {correct: false, question_id:  all_questions.ids}).count
+    (unskipped_answers.where(options: {correct: false, question_id:  all_questions.ids}).count  * 100) / all_questions.count.to_f
   end
 
   def skipped_questions(all_questions)
-    answers.includes(:option).where(options: {name: 'skiped', question_id:  all_questions.ids}).count
+    (answers.includes(:option).where(options: {name: 'skiped', question_id:  all_questions.ids}).count  * 100) / all_questions.count.to_f
   end
 
   def unseen_questions(all_questions)
-    all_questions.where.not(id: questions.ids).count
+    (all_questions.where.not(id: questions.ids).count  * 100) / all_questions.count.to_f
   end
 
   def unskipped_answers
--- app/serializers/api/v1/result_serializer.rb
+++ app/serializers/api/v1/result_serializer.rb
@@ -4,19 +4,19 @@ module Api
       attributes :id, :correct_answers, :wrong_answers, :skipped_questions, :unseen_questions
 
       def correct_answers
-        object.correct_answers(all_questions)
+        object.correct_answers(all_questions).to_s + '%'
       end
 
       def wrong_answers
-        object.wrong_answers(all_questions)
+        object.wrong_answers(all_questions).to_s + '%'
       end
 
       def skipped_questions
-        object.skipped_questions(all_questions)
+        object.skipped_questions(all_questions).to_s + '%'
       end
 
       def unseen_questions
-        object.unseen_questions(all_questions)
+        object.unseen_questions(all_questions).to_s + '%'
       end
 
       def all_questions
